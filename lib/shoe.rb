96a198bdaa1b9339a6c976a1bcb654a1b67ade9c
sronga <sr899@georgetown.edu>
2018-07-24 15:28:06 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,4 @@
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
+  attr_accessor :brand, :color, :size, :material, :condition
+  
 end
